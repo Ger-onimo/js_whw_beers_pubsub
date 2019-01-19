bf096e576227f3040398a5d2e37028dc1b2d102a
Gerry Waterston <gwaterston@hotmail.co.uk>
2019-01-19 12:32:21 +0000

    Fixed list - git push from yesterday not done correctly

--- src/views/beer_view.js
+++ src/views/beer_view.js
@@ -1,28 +1,16 @@
-const BeerView = function() {
+ const BeerView = function (container, beer) {
+  this.beersContainer = container;
+  this.beer = beer;
+}
 
-};
+BeerView.prototype.render = function () {
 
-//  const BeerView = function (container, beer) {
-//   this.beersContainer = container;
-//   this.beer = beer;
-// }
-
-BeerView.prototype.createBeerDetail = function (beer) {
-  const beerDetail = document.createElement('div');
-  beerDetail.classList.add('beer-detail');
-
-// BeerView.prototype.render = function () {
-//
-//   const beersContainer = document.createElement('p');
-//   beersContainer.textContent = this.beer.name;
-//   this.beersContainer.appendChild(beersContainer);
+  const beersContainer = document.createElement('h3');
+  beersContainer.textContent = this.beer.name;
+  this.beersContainer.appendChild(beersContainer);
 
   };
 
-BeerView.prototype.createDetailListItem = function (label, property) {
-  const element = document.createElement('li');
-  element.textContent = `${label}: ${property}`;
-  return element;
-};
+
 
 module.exports = BeerView;
