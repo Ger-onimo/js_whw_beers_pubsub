68ca4b1a7cbd080a00dbf2c4d96c53e587c1a9c6
Gerry Waterston <gwaterston@hotmail.co.uk>
2019-01-20 19:10:45 +0000

    With some css

--- src/views/beer_view.js
+++ src/views/beer_view.js
@@ -3,54 +3,57 @@ const BeerView = function () {
 };
 
   BeerView.prototype.createBeerDetail = function (beer) {
-    // sits inside the html beer-list ul
-    const beerDetail = document.createElement('div');
+// sits inside/under the html beer-list ul
+
+    const beerDetail = document.createElement('section');
     beerDetail.classList.add('beer-detail');
 
-    const name = document.createElement('h3');
-    name.textContent = beer.name;
-    beerDetail.appendChild(name);
+// sits inside/under the html beer-detail section
+    const image = document.createElement('img');
+      image.src = beer.image_url
+      image.height = 100;
+      beerDetail.appendChild(image);
 
-    const detailItem = document.createElement('ul');
-      const description = this.createDetailItem('Description', beer.description);
+    const name = document.createElement('h3');
+      name.textContent = `${beer.name}  (abv: ${beer.abv})`;
+      beerDetail.appendChild(name);
+
+    const tagline = document.createElement('h5');
+      tagline.textContent = `${beer.tagline}`;
+      beerDetail.appendChild(tagline);
+
+
+// sits inside/under name as an unordered list
+    const detailItem = document.createElement('p');
+
+      const descriptionTitle = this.createTextDetail('Description:');
+      detailItem.appendChild(descriptionTitle);
+
+      const description = this.createDetailItem(beer.description);
       detailItem.appendChild(description);
 
-      const abv = this.createDetailItem('abv', beer.abv);
-      detailItem.appendChild(abv);
+      const foodPairingTitle = this.createTextDetail('Good with:');
+      detailItem.appendChild(foodPairingTitle);
 
-      const image = document.createElement('img');
-      image.src = beer.image_url
-      image.height = 200;
-      detailItem.appendChild(image);
-
+      const food_pairing = this.createDetailItem(beer.food_pairing);
+      detailItem.appendChild(food_pairing);
 
   beerDetail.appendChild(detailItem);
   return beerDetail;
 };
 
-  BeerView.prototype.createDetailItem = function (label, property) {
-    const element = document.createElement('li');
-    element.textContent = `${label}: ${property}`;
+
+  BeerView.prototype.createDetailItem = function (property) {
+    const element = document.createElement('ul');
+    element.textContent = `${property}`;
     return element;
 };
 
-module.exports = BeerView;
+BeerView.prototype.createTextDetail = function (label) {
+  const element = document.createElement('h4');
+  element.textContent = `${label}`;
+  return element
 
-// My working end code from Friday
-////////////////////////////////////////////////////
-//  const BeerView = function (container, beer) {
-//   this.beersContainer = container;
-//   this.beer = beer;
-// }
-//
-// BeerView.prototype.render = function () {
-//
-//   const beersContainer = document.createElement('h3');
-//   beersContainer.textContent = this.beer.name;
-//   this.beersContainer.appendChild(beersContainer);
-//
-//   };
-//
-
+};
 
 module.exports = BeerView;
